6ae37988e6cee1280df212e09a1afa92d4a8e1a3
Emily Venturino <ebventurino@gmail.com>
2018-06-03 08:16:39 -0500

    first commit

---
+++ logic.js
@@ -0,0 +1,46 @@
+
+// const piggyBank = {
+//     "quarters": 123,
+//     "nickels": 125,
+//     "dimes": 127,
+//     "pennies": 432,
+// }
+// let penniesAmt = ""
+// let dimesAmt = ""
+// let nickelAmt = ""
+// let quarterAmt = ""
+
+
+// for (let coins in piggyBank) {
+//   penniesAmt = [piggyBank.pennies]/100;
+//   dimesAmt = [piggyBank.dimes]/10;
+//   nickelAmt = [piggyBank.nickels]/5;
+//   quarterAmt = [piggyBank.quarters]/25;
+
+
+//     console.log(`I have $${penniesAmt} in pennies, $${dimesAmt} in dimes, $${nickelAmt} in nickels, and $${quarterAmt} in quarters.`)
+    
+// }
+
+//                                     CHALLENGE 2
+// const piggyBank = {
+//     "quarters": 1.25,
+//     "nickels": 0.25,
+//     "dimes": 1.30,
+//     "pennies": 4.32,
+// }
+// let penniesAmt = ""
+// let dimesAmt = ""
+// let nickelAmt = ""
+// let quarterAmt = ""
+
+
+// for (let coins in piggyBank) {
+//   penniesAmt = 100 * [piggyBank.pennies];
+//   dimesAmt = 10*[piggyBank.dimes];
+//   nickelAmt = 20*[piggyBank.nickels];
+//   quarterAmt = 4*[piggyBank.quarters];
+
+
+//     console.log(`I have ${penniesAmt}  pennies, ${dimesAmt} dimes, ${nickelAmt} nickels, and ${quarterAmt} quarters.`)
+    
